19c8227f3c399304d2109d87e51790dd99dcf5d2
Maarten Behn <46872913+MaartenBehn@users.noreply.github.com>
2023-04-27 14:40:28 +0200

    found bug

---
+++ test_file_creater/c_compare.py
@@ -0,0 +1,36 @@
+import json
+import os
+import numpy as np
+from distance3d import colliders, random
+from distance3d.gjk._gjk_nesterov_accelerated import gjk_nesterov_accelerated
+
+iterations = 100
+random_state = np.random.RandomState(84)
+shape_names = ["sphere", "capsule", "cylinder"]
+
+original_iteration_sum = 0
+jolt_iteration_sum = 0
+nasterov_iteration_sum = 0
+
+for i in range(iterations):
+
+
+    shape1 = shape_names[random_state.randint(len(shape_names))]
+    args1 = random.RANDOM_GENERATORS[shape1](random_state)
+    shape2 = shape_names[random_state.randint(len(shape_names))]
+    args2 = random.RANDOM_GENERATORS[shape2](random_state)
+    collider1 = colliders.COLLIDERS[shape1](*args1)
+    collider2 = colliders.COLLIDERS[shape2](*args2)
+
+    collider1.round_values(6)
+    collider2.round_values(6)
+
+    if i == 3:
+        print("Case:", i)
+        nasterov_iterations = gjk_nesterov_accelerated(collider1, collider2,
+                                                       ray_guess=None,
+                                                       max_interations=100,
+                                                       upper_bound=1.79769e+308,
+                                                       tolerance=1e-6,
+                                                       inflation=1.07485)[3]
+        print("Nasterov Intertions", nasterov_iterations)
