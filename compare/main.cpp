fa80b54800e9a857538170fe7acf6c394cd3fee6
Maarten Behn <maarten.behn@gmail.com>
2023-05-18 13:41:37 +0200

    collider json loading

--- compare/main.cpp
+++ compare/main.cpp
@@ -12,7 +12,7 @@ using json = nlohmann::json;
 enum ColliderType {
 	Sphere,
 	Capsule,
-	Zylinder,
+	Cylinder,
 	Box,
 };
 
@@ -45,25 +45,6 @@ float getSizeZ(Collider collider){
 Collider parseCollider(json collider_json){
 
 	Collider collider;
-	collider.colliderToOrigen[0] = collider_json["collider2origin"][0][0];
-	collider.colliderToOrigen[1] = collider_json["collider2origin"][0][1];
-	collider.colliderToOrigen[2] = collider_json["collider2origin"][0][2];
-	collider.colliderToOrigen[3] = collider_json["collider2origin"][0][3];
-
-	collider.colliderToOrigen[4] = collider_json["collider2origin"][1][0];
-	collider.colliderToOrigen[5] = collider_json["collider2origin"][1][1];
-	collider.colliderToOrigen[6] = collider_json["collider2origin"][1][2];
-	collider.colliderToOrigen[7] = collider_json["collider2origin"][1][3];
-
-	collider.colliderToOrigen[8] = collider_json["collider2origin"][2][0];
-	collider.colliderToOrigen[9] = collider_json["collider2origin"][2][1];
-	collider.colliderToOrigen[10] = collider_json["collider2origin"][2][2];
-	collider.colliderToOrigen[11] = collider_json["collider2origin"][2][3];
-
-	collider.colliderToOrigen[12] = collider_json["collider2origin"][3][0];
-	collider.colliderToOrigen[13] = collider_json["collider2origin"][3][1];
-	collider.colliderToOrigen[14] = collider_json["collider2origin"][3][2];
-	collider.colliderToOrigen[15] = collider_json["collider2origin"][3][3];
 
 	auto type = collider_json["typ"];
 	if (type == "Sphere"){
@@ -77,26 +58,47 @@ Collider parseCollider(json collider_json){
 		collider.data[1] = collider_json["height"];
 	}
 
-	if (type == "Zylinder"){
-		collider.type = ColliderType::Zylinder;	
+	if (type == "Cylinder"){
+		collider.type = ColliderType::Cylinder;
 		collider.data[0] = collider_json["radius"];
 		collider.data[1] = collider_json["height"];
 	}
 
 	if (type == "Box"){
-		collider.type = ColliderType::Box;	
-		
+		collider.type = ColliderType::Box;
+        collider.data[0] = collider_json["size"][0];
+        collider.data[1] = collider_json["size"][1];
+        collider.data[2] = collider_json["size"][2];
 	}
+
+    collider.colliderToOrigen[0] = collider_json["collider2origin"][0][0];
+    collider.colliderToOrigen[1] = collider_json["collider2origin"][0][1];
+    collider.colliderToOrigen[2] = collider_json["collider2origin"][0][2];
+    collider.colliderToOrigen[3] = collider_json["collider2origin"][0][3];
+
+    collider.colliderToOrigen[4] = collider_json["collider2origin"][1][0];
+    collider.colliderToOrigen[5] = collider_json["collider2origin"][1][1];
+    collider.colliderToOrigen[6] = collider_json["collider2origin"][1][2];
+    collider.colliderToOrigen[7] = collider_json["collider2origin"][1][3];
+
+    collider.colliderToOrigen[8] = collider_json["collider2origin"][2][0];
+    collider.colliderToOrigen[9] = collider_json["collider2origin"][2][1];
+    collider.colliderToOrigen[10] = collider_json["collider2origin"][2][2];
+    collider.colliderToOrigen[11] = collider_json["collider2origin"][2][3];
+
+    collider.colliderToOrigen[12] = collider_json["collider2origin"][3][0];
+    collider.colliderToOrigen[13] = collider_json["collider2origin"][3][1];
+    collider.colliderToOrigen[14] = collider_json["collider2origin"][3][2];
+    collider.colliderToOrigen[15] = collider_json["collider2origin"][3][3];
+
 
 	return collider;
 }
 
 
-
-
 void load() {
 
-	std::ifstream f("./data/test_data.json");
+	std::ifstream f("../../data/test_data.json");
 	json data = json::parse(f);
 
 	for (auto collide_case : data)
@@ -122,13 +124,13 @@ void load() {
 
 int main(){
 
+    load();
+
 	// jolt_hello_world();
 
-	fcl_hello_world();
+	// fcl_hello_world();
 
-	// load();
-
-	bullet_hello_world();
+	// bullet_hello_world();
 
 	return 0;
 }
