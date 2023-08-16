03282ebaf12d3da0546c0df1ac414efc5d18d3d5
Maarten Behn <maarten.behn@gmail.com>
2023-08-16 13:51:22 +0200

    build fix

--- scripts/compile/compile_compare_release.sh
+++ scripts/compile/compile_compare_release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd compare-cpp/build_release/
-cmake -DBUILD_PYTHON_INTERFACE:BOOL=OFF -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . --target compare -j 8
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=ninja -DBUILD_PYTHON_INTERFACE:BOOL=OFF -G Ninja -S .. -B .
+cmake --build . --target compare -j 6
 cd ../..
 
