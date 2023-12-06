2b53178c3ae4345cd73ee0bc7658409be62a216c
Maarten Behn <maarten.behn@gmail.com>
2023-12-06 18:31:48 +0100

    fix

--- compare-python/analyze/analyze_results_archive.py
+++ compare-python/analyze/analyze_results_archive.py
@@ -119,6 +119,9 @@ for pc_name in os.listdir(result_path):
             print("statistics: ", statistics)
             print("p: ", p)
 
+            d = p * np.sqrt((1 / len(result_a)) + (1 / len(result_b)))
+            print("D: ", d)
+
         #  Violin Plot
         pos = []
         data = []
