6bb23931f2194ac6c3d4619cb10956c468c299b0
Maarten Behn <maarten.behn@gmail.com>
2023-12-06 19:13:02 +0100

    plot

--- compare-python/analyze/analyze_results_archive.py
+++ compare-python/analyze/analyze_results_archive.py
@@ -113,17 +113,50 @@ for pc_name in os.listdir(result_path):
 
         # T test
         print("\n -- T Test --")
-        for (key_a, result_a), (key_b, result_b) in [(a, b) for idx, a in enumerate(results.items()) for b in list(results.items())[idx + 1:]]:
-            print(key_a, " vs ", key_b)
-            statistics, p = scipy.stats.ttest_ind(result_a, result_b)
-            print("statistics: ", statistics)
-            print("p: ", p)
 
-            d = statistics * np.sqrt((1 / len(result_a)) + (1 / len(result_b)))
-            print("D with statistics as t: ", d)
+        d_results = []
+        i = 0
+        for key_a, result_a in results.items():
+            d_results.append([])
 
-            d = p * np.sqrt((1 / len(result_a)) + (1 / len(result_b)))
-            print("D with p as t: ", d)
+            for key_b, result_b in results.items():
+                print(key_a, " vs ", key_b)
+                statistics, p = scipy.stats.ttest_ind(result_a, result_b)
+                d = statistics * np.sqrt((1 / len(result_a)) + (1 / len(result_b)))
+
+                print("statistics: ", statistics)
+                print("p: ", p)
+                print("D with statistics as t: ", d)
+
+                d_results[i].append(d)
+
+            i += 1
+
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+
+        plt.imshow(results)
+        ax.set_title(f"{name} on {pc_name}")
+        ax.set_aspect('equal')
+
+        short_names = get_short_names()
+        plt.xticks(np.arange(0, len(short_names.values()), 1.0))
+        plt.yticks(np.arange(0, len(short_names.values()), 1.0))
+        ax.set_xticklabels(short_names.values())
+        ax.set_yticklabels(short_names.values())
+        plt.xticks(rotation=90, ha='right')
+
+        cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+        cax.get_xaxis().set_visible(False)
+        cax.get_yaxis().set_visible(False)
+        cax.patch.set_alpha(0)
+        cax.set_frame_on(False)
+        plt.colorbar(orientation='vertical')
+
+        fig.tight_layout()
+        plt.show()
+        plt.savefig(f"{name}_on_{pc_name}_d.png")
+
 
         #  Violin Plot
         pos = []
@@ -157,7 +190,7 @@ for pc_name in os.listdir(result_path):
         ax.set_title(f"{name} on {pc_name}")
         fig.tight_layout()
         plt.show()
-        plt.savefig(f"{name} on {pc_name}.png")
+        plt.savefig(f"{name}_on_{pc_name}_violin.png")
 
 
 
