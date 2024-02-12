b9d0093be78bb81c95f1b1145ef0d4f162001fce
Alexander Fabisch <alexander.fabisch@dfki.de>
2024-02-12 11:35:23 +0100

    Compile UC2 collision cases

---
+++ compare-python/debug/show_uc2_ur10.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pytransform3d.visualizer as pv
+
+from src import get_uc2_bvh, get_ur10_tm, get_ur10_bvh_from_tm, uc2_ur10_offset, set_random_joints
+
+uc2_tm, uc2_bvh = get_uc2_bvh()
+ur10_tm = get_ur10_tm()
+set_random_joints(ur10_tm)
+ur10_bvh = get_ur10_bvh_from_tm(ur10_tm, base_frame2origin=uc2_ur10_offset)
+
+fig = pv.figure()
+for artist in uc2_bvh.get_artists():
+    artist.add_artist(fig)
+
+for artist in ur10_bvh.get_artists():
+    artist.add_artist(fig)
+
+if "__file__" in globals():
+    fig.show()
+else:
+    fig.save_image("__open3d_rendered_image.jpg")
